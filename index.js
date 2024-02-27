d50830639788db4d7c24a9e0c52af081133cbfbe
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:27:17 -0800

    Famous quote 5
    Typescript 45 Q exercise 5

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Quaid-e-Azam once said"Expect the best, prepare for the worst"');
+let famous_person = "Quaid-e-Azam";
+let message = famous_person + 'Quaid-e-Azam once said"Expect the best, prepare for the worst"';
+console.log(message);
